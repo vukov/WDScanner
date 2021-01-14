5ea01e63afd74819201d9c5ff6612a615887dffd
vukov <75134922+vukov@users.noreply.github.com>
2021-01-14 12:29:18 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,3 +2,90 @@ window.onload = () => {
   'use strict';
   console.log("[js/app.js] app.js onload");
 }
+
+let filters = [];
+filters.push({namePrefix: "Fireseatrack"});
+
+
+const scan_options = {
+    keepRepeatedDevices: true,
+    filters: filters
+};
+
+
+async function onButtonClick() {
+  try {
+    log('Requesting Bluetooth Scan with options: ' + JSON.stringify(scan_options));
+    const scan = await navigator.bluetooth.requestLEScan(scan_options);
+
+    log('Scan started with:');
+    log(' acceptAllAdvertisements: ' + scan.acceptAllAdvertisements);
+    log(' active: ' + scan.active);
+    log(' keepRepeatedDevices: ' + scan.keepRepeatedDevices);
+    log(' filters: ' + JSON.stringify(scan.filters));
+
+    navigator.bluetooth.addEventListener('advertisementreceived', event => {
+      log('Advertisement received.');
+      log('  Device Name: ' + event.device.name);
+      log('  Device ID: ' + event.device.id);
+      log('  RSSI: ' + event.rssi);
+      log('  TX Power: ' + event.txPower);
+      log('  UUIDs: ' + event.uuids);
+      event.manufacturerData.forEach((valueDataView, key) => {
+        logDataView('Manufacturer', key, valueDataView);
+      });
+      event.serviceData.forEach((valueDataView, key) => {
+        logDataView('Service', key, valueDataView);
+      });
+    });
+
+    setTimeout(stopScan, 10000);
+    function stopScan() {
+      log('Stopping scan...');
+      scan.stop();
+      log('Stopped.  scan.active = ' + scan.active);
+    }
+  } catch(error)  {
+    log('Argh! ' + error);
+  }
+}
+
+/* Utils */
+
+const logDataView = (labelOfDataSource, key, valueDataView) => {
+  const hexString = [...new Uint8Array(valueDataView.buffer)].map(b => {
+    return b.toString(16).padStart(2, '0');
+  }).join(' ');
+  const textDecoder = new TextDecoder('ascii');
+  const asciiString = textDecoder.decode(valueDataView.buffer);
+  log(`  ${labelOfDataSource} Data: ` + key +
+      '\n    (Hex) ' + hexString +
+      '\n    (ASCII) ' + asciiString);
+};
+
+
+
+  log: function() {
+      var line = Array.prototype.slice.call(arguments).map(function(argument) {
+        return typeof argument === 'string' ? argument : JSON.stringify(argument);
+      }).join(' ');
+
+      document.querySelector('#log').textContent += line + '\n';
+    },
+
+    clearLog: function() {
+      document.querySelector('#log').textContent = '';
+    },
+
+    setStatus: function(status) {
+      document.querySelector('#status').textContent = status;
+    },
+
+    setContent: function(newContent) {
+      var content = document.querySelector('#content');
+      while(content.hasChildNodes()) {
+        content.removeChild(content.lastChild);
+      }
+      content.appendChild(newContent);
+    }
+  };
